b2fa479bcfa1f9bba019f1c3f10b1c1baf9a3fe9
CamilaBaron2 <camiilaapaez12@gmail.com>
2022-12-18 18:32:21 -0500

    primer commit

---
+++ src/main/java/com/chukwudi/springboot/chukwudi/ChukwudiApplication.java
@@ -0,0 +1,13 @@
+package com.chukwudi.springboot.chukwudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChukwudiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChukwudiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chukwudi/springboot/chukwudi/ChukwudiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chukwudi.springboot.chukwudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChukwudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
